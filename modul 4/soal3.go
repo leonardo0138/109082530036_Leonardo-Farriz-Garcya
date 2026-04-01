84235a6ddb6ee8c6a8d58db5e7a39297ce3d5a03
leonardo0138 <leonardofarriz19@gmail.com>
2026-04-01 13:51:20 +0700

    y

--- modul 4/soal3.go
+++ modul 4/soal3.go
@@ -1,4 +1,24 @@
 package main
 
+import "fmt"
+
+func deret(n int) {
+	for {
+		fmt.Print(n, " ")
+		if n == 1 {
+			break
+		}
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+}
+
 func main() {
-	
+	var n int
+	fmt.Scan(&n)
+
+	deret(n)
+}
